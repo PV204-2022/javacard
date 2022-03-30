f31db5b76c25d84d9ffb75d81059633525b32dc8
Ondřej Molík <ondrejmo@users.noreply.github.com>
2022-03-30 13:13:29 +0200

    Add basic SecretItem class

--- applet/src/main/java/applet/SecretItem.java
+++ applet/src/main/java/applet/SecretItem.java
@@ -1,13 +1,38 @@
 package applet;
 
+import javacard.framework.Util;
+
+/**
+ * Stores exactly one value
+ */
 public class SecretItem {
-
-
-  public void getValue() {
-    return;
+  private final bytes[] value;
+  
+  /**
+   * Just a basic constructor
+   *
+   */
+  public SecretItem() {
+    value = new bytes[Configuration.SECRET_VALUE_MAX_LENGTH];
   }
 
-  public void setValue() {
-    return;
+  /**
+   * A Getter
+   * @param dest - Where do we put it
+   */
+  public void getValue(byte[] dst) {
+    Util.arrayCopy(value, (short) 0, dst, (short) 0, Configuration.SECRET_VALUE_MAX_LENGTH);
+  }
+
+  /**
+   * A Setter
+   * @param src - What do we store
+   */
+  public void setValue(byte[] src) {
+    if (src.length > SECRET_VALUE_MAX_LENGTH) {
+      throw new UserException();
+    }
+    
+    Util.arrayCopy(src, (short) 0, value, (short) 0, Configuration.SECRET_VALUE_MAX_LENGTH);
   }
 }
