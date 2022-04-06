8e49e7bf33b1132ad54411fbe2e76d6413918f86
Vojtěch Jelínek <vojtin.j@gmail.com>
2022-04-06 23:13:02 +0200

    Custom PIN

--- applet/src/main/java/applet/CustomPIN.java
+++ applet/src/main/java/applet/CustomPIN.java
@@ -1,12 +1,44 @@
 package applet;
 
-import javacard.framework.OwnerPINx;
+import javacard.framework.OwnerPIN;
+import javacard.framework.PINException;
 
-public class CustomPIN extends OwnerPINx {
+public class CustomPIN {
 
-    public CustomPIN(byte tryLimit, byte maxPINSize) throws PINException
-        super(tryLimit, maxPINSize)
-    	register();
+    public static final byte PIN_STATUS_CORRECT = (byte) 0;
+    public static final byte PIN_STATUS_INCORRECT = (byte) 1;
+    public static final byte PIN_STATUS_DURESS = (byte) 2;
+
+    final private OwnerPIN pin;
+    private byte[] duressPin;
+    final private byte pinMaxLength;
+
+    public CustomPIN(byte retries, byte pinMaxLength) {
+        this.pin = new OwnerPIN(retries, pinMaxLength);
+        this.duressPin = new byte[pinMaxLength];
+        this.pinMaxLength = pinMaxLength;
 	}
+
+    public void initPin(byte[] pinValue, byte pinLength) throws PINException {
+        this.pin.update(pinValue, (short) 0, pinLength);
+    }
+
+    public void initDuressPin(byte[] pinValue, byte pinLength) throws PINException {
+        if (pinLength > this.pinMaxLength) {
+            throw new PINException(PINException.ILLEGAL_VALUE);
+        }
+        this.duressPin = pinValue;
+    }
+
+    public byte check(byte[] pinValue, byte pinLength) throws PINException {
+        if (!this.pin.check(pinValue, (short) 0, pinLength)) {
+            if (pinValue == this.duressPin) {
+                return PIN_STATUS_DURESS;
+            } else {
+                return PIN_STATUS_INCORRECT;
+            }
+        }
+        return PIN_STATUS_CORRECT;
+    }
 
 }
